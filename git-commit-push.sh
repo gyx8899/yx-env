9ed1c3b4e8a2e73c946ba3abe2f483704e4d2c6d
Yingxin Guo <yingxin.guo@neulion.com.cn>
2020-05-06 16:33:15 +0800

    chore(package.json): updated;

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 hasChange=""
-
+echo "before check"
 if [[ -n $(git diff HEAD --stat) ]]; then
   hasChange="local-diff"
   . ./git-commit.sh
-elif [ $(git status) ]; then
-
+elif [[ $(git status) == *"Your branch is ahead of"* ]]; then
+  hasChange="commit-diff"
 fi
 
 if [[ $hasChange != '' ]]; then
   git pull --rebase
-  git push
+#  git push
 fi
 
-#exec bash
+exec bash
