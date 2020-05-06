c52d8d8d727a16f87b485eb385bf3fa5a93bed30
Yingxin Guo <yingxin.guo@neulion.com.cn>
2020-05-06 18:18:22 +0800

    chore(git-registry-taobao.sh): updated;

--- git-registry-taobao.sh
+++ git-registry-taobao.sh
@@ -5,3 +5,6 @@ npm config get registry
 
 # set config: 淘宝镜像
 npm config set registry https://registry.npm.taobao.org
+
+# default config (can be reset after set with taobao)
+#npm config set registry https://registry.npmjs.org
