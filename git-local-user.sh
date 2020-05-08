1c50962ff6b4ade0cb198855d1624becd48c80bf
Steper Kuo <gyx8899@126.com>
2020-05-08 18:18:43 +0800

    feature(yx-env.iml): added; chore(git-local-user.sh): updated;

--- git-local-user.sh
+++ git-local-user.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-gitResetLocalUser(){
+gitPullRebase(){
   if [ -d "$1" ] && [ ! -L "$1" ] && [ ! -d .git ] ; then
     cd $1
     echo $1
 
+#    git config user.name
+#    git config user.email
+
+#    git config --local user.name
+#    git config --local user.email
+
     git config --local user.name "Yingxin Guo"
     git config --local user.email "yingxin.guo@neulion.com.cn"
 
+#    git config --local --unset user.name
+#    git config --local --unset user.email
+
 #    Divider
-    echo ""
-    echo ""
+    echo "user.name Yingxin Guo"
+    echo "user.email yingxin.guo@neulion.com.cn"
     echo ""
   fi
 
@@ -19,12 +28,15 @@ gitResetLocalUser(){
   return 0
 }
 
+currentFolder=$(basename $PWD)
 cd ..
 if [ -n "$1" ]; then
-  gitResetLocalUser $1
+  gitPullRebase $1
 else
   for f in *; do
-    gitResetLocalUser $f
+    if [[ $f != $currentFolder ]]; then
+        gitPullRebase $f
+    fi
   done
 fi
 
