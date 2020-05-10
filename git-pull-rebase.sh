7dbd3619ef0d64a3af2deda2db023d3aa420a821
Steper Kuo <gyx8899@126.com>
2020-05-10 21:53:22 +0800

    chore(git-pull-rebase.sh): updated;

--- git-pull-rebase.sh
+++ git-pull-rebase.sh
@@ -2,9 +2,9 @@
 
 gitPullRebase(){
   # 1. 项目目录存在，2.非符号链接目录, 3.有 git 目录
-  if [ -d "$2/$1" ] && [ ! -L "$2/$1" ] && [ -d "$2/$1/.git" ] ; then
-    cd $1
-    echo "$1"
+  if [ -d "$1" ] && [ ! -L "$1" ] && [ -d "$1/.git" ] ; then
+    cd $2
+    echo "$2"
     echo ""
     hasDiff=false
     if [[ -n $(git diff HEAD --stat) ]]; then
@@ -30,10 +30,10 @@ gitPullRebase(){
 
 cd ..
 if [ -n "$1" ]; then
-  gitPullRebase $1 $PWD
+  gitPullRebase "$PWD/$1" $1
 else
   for f in *; do
-    gitPullRebase $f $PWD
+    gitPullRebase "$PWD/$f" $f
   done
 fi
 
