4e929f7545ae923782fbd4235c892dca0b76ae66
Steper Kuo <gyx8899@126.com>
2020-11-16 16:57:40 +0800

    :art: chore(git-push.sh): updated;

--- git-push.sh
+++ git-push.sh
@@ -103,7 +103,7 @@ if ((changes)); then
     if [[ $commitmsg != '' ]]; then
       commitmsg+=$newline
     fi
-    commitmsg+=":fire: refactor($copied): copied;"
+    commitmsg+="✂ 📋 refactor($copied): copied;"
   fi
 
   echo "$commitmsg"
