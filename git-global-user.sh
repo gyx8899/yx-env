41fcaf79eb9eee077383a7dfd3986b3f27e2da3b
Steper Kuo <gyx8899@126.com>
2020-11-23 14:12:00 +0800

    :art: chore(git-global-user.sh): updated;

--- git-global-user.sh
+++ git-global-user.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+git config --global --unset user.name
+git config --global --unset user.email
+
 git config --global user.name "Steper Kuo"
 git config --global user.email "gyx8899@126.com"
