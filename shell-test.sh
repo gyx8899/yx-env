73375142899e25938d874ac6f93709117bb00300
Steper Kuo <gyx8899@126.com>
2021-01-22 18:52:37 +0800

    :art: chore(shell-test.sh): updated;

--- shell-test.sh
+++ shell-test.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-#git status
+echo "Start"
 
-git pull --rebase
-# Current branch master is up to date.
+git pull --rebase | (
+    echo "00"
+    while read -r line; do
+      echo "1 - $line"
+    done
+    echo "22"
+)
 
-while read -r gitPullRebaseInfo; do
-  echo "$gitPullRebaseInfo"
-  if [ "${gitPullRebaseInfo}" == 'Current branch master is up to date.' ]; then
-    git push
-    exit
-  else
-    echo "Something wrong! Please have a check."
-    exec bash
-  fi
-done < <(git pull --rebase)
+git status | (
+    unset dirty deleted untracked newfile ahead renamed
+    while read line ; do
+        case "$line" in
+          *modified:*)                      dirty='!' ; ;;
+          *deleted:*)                       deleted='x' ; ;;
+          *'Untracked files:')              untracked='?' ; ;;
+          *'new file:'*)                    newfile='+' ; ;;
+          *'Your branch is ahead of '*)     ahead='*' ; ;;
+          *renamed:*)                       renamed='>' ; ;;
+        esac
+    done
+    bits="$dirty$deleted$untracked$newfile$ahead$renamed"
+    [ -n "$bits" ] && echo " $bits" || echo
+)
+
+echo "End"
+
+exec bash
