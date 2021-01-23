5a8cca534935e5e1cad1d5a1b0249798efe84e65
Yingxin Guo <yingxin.guo@neulion.com.cn>
2021-01-24 00:34:16 +0800

    :art: chore(git-merge-remote.sh): updated;

--- git-merge-remote.sh
+++ git-merge-remote.sh
@@ -7,23 +7,28 @@ if [ "$1" ]; then
   cd ../$1
 fi
 
+branchName="master"
+if [ "$3" ]; then
+  branchName=$3
+fi
+
 #git pull --unshallow
 git fetch --depth=10000
 git remote remove upstream
 git remote add upstream $2
 git fetch upstream
-git checkout master
-git merge --no-edit upstream/master --allow-unrelated-histories
+git checkout $branchName
+git merge --no-edit upstream/$branchName --allow-unrelated-histories
 git checkout --theirs .
 git add .
 
 if [[ -z $(git diff HEAD --stat) ]]; then
-  if [[ -z $(git log origin/master..HEAD) ]]; then
+  if [[ -z $(git log origin/$branchName..HEAD) ]]; then
      exit
   fi
 fi
 
 git commit -m ":repeat: Sync from $2"
-git push origin master
+git push origin $branchName
 
 exit
