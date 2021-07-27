c2536a89eb4db6cef6531446159ed25a30067fcd
KateKuo <gyx8899@126.com>
2021-07-27 12:28:48 +0800

    :art: chore(git-local-user.sh/package-lock.json/package.json): updated;

--- git-local-user.sh
+++ git-local-user.sh
@@ -2,23 +2,26 @@
 
 gitSetConfig(){
   # [ ! -d .git ] 当前目录是 git 目录
+
   if [ -d "$1" ] && [ ! -L "$1" ] && [ ! -d .git ] ; then
     cd $1
-
-    if [ -d "$2" ] && [ ! -L "$2" ] &&[ -d "$3" ] && [ ! -L "$3" ] ; then
-      echo ""
-      echo "Before update local user.name :" $(git config --local user.name)
+    if [ -n "$2" ] ; then
       echo "Before update local user.email:" $(git config --local user.email)
 
       git config --local user.name $2
-      git config --local user.email $3
-
   #    git config --local --unset user.name
-  #    git config --local --unset user.email
 
   #    Divider
       echo ""
       echo "After update local user.name  :" $(git config --local user.name)
+    fi
+    if [ -n "$3" ] ; then
+      echo "Before update local user.email:" $(git config --local user.email)
+
+      git config --local user.email $3
+  #    git config --local --unset user.email
+
+  #    Divider
       echo "After update local user.email :" $(git config --local user.email)
     fi
   fi
@@ -28,16 +31,21 @@ gitSetConfig(){
   return 0
 }
 
+echo $1 $2 $3
+
 currentFolder=$(basename $PWD)
 cd ..
+
+# [ -n string ]：如果字符串string的长度大于零，则判断为真
 if [ -n "$1" ]; then
+  echo 'gitSetConfig' $1 $2 $3
   gitSetConfig $1 $2 $3
 else
   for f in *; do
     if [[ $f != $currentFolder ]]; then
-        gitSetConfig $f
+        gitSetConfig $f $2 $3
     fi
   done
 fi
 
-#exec bash
+exec bash
