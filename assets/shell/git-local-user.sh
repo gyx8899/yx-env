b16b72693a6ae05b6b2adcacc998efa06ef7188e
KateKuo <gyx8899@126.com>
2022-03-03 11:23:05 +0800

    :art: chore(git-local-user.sh): updated;

--- assets/shell/git-local-user.sh
+++ assets/shell/git-local-user.sh
@@ -2,18 +2,22 @@
 
 gitSetConfig(){
   # [ ! -d .git ] 当前目录是 git 目录
+  # [ -L file ] 如果 file 存在并且是一个符号链接，则为 true
 
-  if [ -d "$1" ] && [ ! -L "$1" ] && [ ! -d .git ] ; then
+  # if [ -d "$1" ] && [ ! -L "$1" ] && [ ! -d .git ] ; then
+  if [ -d "$1" ] && [ ! -d .git ] ; then
     cd $1
+    echo ""
+    echo "path: ${PWD}"
     if [ -n "$2" ] ; then
-      echo "Before update local user.email:" $(git config --local user.email)
+      
+      echo "Before update local user.name:" $(git config --local user.email)
 
       git config --local user.name $2
   #    git config --local --unset user.name
 
   #    Divider
-      echo ""
-      echo "After update local user.name  :" $(git config --local user.name)
+      echo "After  update local user.name:" $(git config --local user.name)
     fi
     if [ -n "$3" ] ; then
       echo "Before update local user.email:" $(git config --local user.email)
@@ -22,8 +26,9 @@ gitSetConfig(){
   #    git config --local --unset user.email
 
   #    Divider
-      echo "After update local user.email :" $(git config --local user.email)
+      echo "After  update local user.email:" $(git config --local user.email)
     fi
+    echo ""
   fi
 
   cd ..
@@ -34,14 +39,16 @@ gitSetConfig(){
 echo $1 $2 $3
 
 currentFolder=$(basename $PWD)
-cd ..
+# 当前文件的路径 yx-env/assets/shell，需要跳到项目目录
+cd ../../../
 
 # [ -n string ]：如果字符串string的长度大于零，则判断为真
 if [ -n "$1" ]; then
-  echo 'gitSetConfig' $1 $2 $3
+  echo 'gitSetConfig' $1 $2 $3 
   gitSetConfig $1 $2 $3
 else
   for f in *; do
+    echo "*: $f"
     if [[ $f != $currentFolder ]]; then
         gitSetConfig $f $2 $3
     fi
