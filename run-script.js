84f3ec0ac772c62d0e5d42e90a3f646c9f062d81
KateKuo <gyx8899@126.com>
2022-07-17 14:44:32 +0800

    :art: chore: run-script.js updated;

--- run-script.js
+++ run-script.js
@@ -6,4 +6,6 @@ console.log('getNodeArgs', fileName, others)
 const prefix = fileName.endsWith('.sh') ? (isWindowsOs() ? '' : 'bash ') : ''
 const suffix = fileName.endsWith('.sh') ? Object.keys(others).map(key=>others[key]).join(' ') : Object.keys(others).map(key=>`--${key}=${others[key]}`).join(' ')
 
+console.log('prefix: ', prefix, 'suffix: ', suffix)
+
 runFile(`${prefix}${fileName} ${suffix}`)
