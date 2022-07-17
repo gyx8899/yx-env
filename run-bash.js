28654cdbebc2ec91eb49a2609a7ab0cd0aa933ea
KateKuo <gyx8899@126.com>
2022-07-17 14:03:07 +0800

    :art: chore: run-bash.js updated;

--- run-bash.js
+++ run-bash.js
@@ -1,9 +1,6 @@
 const {isWindowsOs, getNodeArgs, runFile} = require('./assets/libs/utils')
 
 
-console.log(`isWindowsOs: ${isWindowsOs()}`)
-
-
 const {fileName, ...others} = getNodeArgs()
 console.log('getNodeArgs', fileName, others)
 runFile(`${isWindowsOs() ? '' : 'bash '}${fileName}`)
